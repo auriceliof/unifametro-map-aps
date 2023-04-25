437e8386f29a98519d6ad4d1f1245f08ef2960ba
auriceliof <auricelio.suporte@hotmail.com>
2023-04-24 22:11:59 -0300

    Tirar_fundo and Normalizar

--- tela_login/login.py
+++ tela_login/login.py
@@ -1,7 +1,6 @@
 from PyQt5 import  uic,QtWidgets
 import sqlite3
 
-
 def chama_segunda_tela():
     primeira_tela.label_4.setText("")
     nome_usuario = primeira_tela.lineEdit.text()
@@ -11,19 +10,31 @@ def chama_segunda_tela():
         segunda_tela.show()
     else :
         primeira_tela.label_4.setText("Dados de login incorretos!")
-    
 
 def logout():
     segunda_tela.close()
     primeira_tela.show()
 
+def tirar_fundo():
+    exec(open("categorias/TirarFundo.py").read())
+
+def Normalizar():
+    exec(open("categorias/Normalizar.py").read())
 
 app=QtWidgets.QApplication([])
 primeira_tela=uic.loadUi("primeira_tela.ui")
 segunda_tela = uic.loadUi("segunda_tela.ui")
 primeira_tela.pushButton.clicked.connect(chama_segunda_tela)
-segunda_tela.pushButton.clicked.connect(logout)
+
+segunda_tela.pushButton.clicked.connect(tirar_fundo)
+segunda_tela.pushButton_2.clicked.connect(Normalizar)
 primeira_tela.lineEdit_2.setEchoMode(QtWidgets.QLineEdit.Password)
 
 primeira_tela.show()
 app.exec()
+
+
+
+
+
+
