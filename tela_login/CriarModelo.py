d042f89a1495bbf85de6439575340a5438105222
auriceliof <auricelio.suporte@hotmail.com>
2023-04-28 20:33:21 -0300

    Criar Modelo - OK

--- tela_login/CriarModelo.py
+++ tela_login/CriarModelo.py
@@ -62,20 +62,29 @@ print("Labels : ", len(labels))
 
 x_train, x_test, y_train, y_test = train_test_split(images, labels, test_size=0.2)
 
-def extract_features(image):
+
+x_train_features = []
+x_test_features = []
+
+for image in x_train:
     color_hist = cv2.calcHist([image], [0, 1, 2], None, [8, 8, 8], [0, 256, 0, 256, 0, 256])
     color_hist = cv2.normalize(color_hist, color_hist).flatten()
-    return color_hist
+    x_train_features.append(color_hist)
 
-x_train_features = [extract_features(image) for image in x_train]
+for image in x_test:
+    color_hist = cv2.calcHist([image], [0, 1, 2], None, [8, 8, 8], [0, 256, 0, 256, 0, 256])
+    color_hist = cv2.normalize(color_hist, color_hist).flatten()
+    x_test_features.append(color_hist)
 
-x_test_features = [extract_features(image) for image in x_test]
+
 
 clf = SVC(kernel='linear', C=1000)
+
 
 clf.fit(x_train_features, y_train)
 
 acuracia = clf.score(x_test_features, y_test)
+
 
 print("Acuracia : ", acuracia)
 
