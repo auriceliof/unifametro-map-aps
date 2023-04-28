75c6907364bcbc85d58a34fd25f80f55b87a50b3
auriceliof <auricelio.suporte@hotmail.com>
2023-04-28 10:17:57 -0300

    update

--- tela_login/login.py
+++ tela_login/login.py
@@ -15,19 +15,24 @@ def logout():
     segunda_tela.close()
     primeira_tela.show()
 
-def tirar_fundo():
+def TirarFundo():
     exec(open("categorias/TirarFundo.py").read())
 
 def Normalizar():
     exec(open("categorias/Normalizar.py").read())
+
+def CriarModelo():
+    exec(open("categorias/CriarModelo.py").read())
+
 
 app=QtWidgets.QApplication([])
 primeira_tela=uic.loadUi("primeira_tela.ui")
 segunda_tela = uic.loadUi("segunda_tela.ui")
 primeira_tela.pushButton.clicked.connect(chama_segunda_tela)
 
-segunda_tela.pushButton.clicked.connect(tirar_fundo)
+segunda_tela.pushButton.clicked.connect(TirarFundo)
 segunda_tela.pushButton_2.clicked.connect(Normalizar)
+segunda_tela.pushButton_3.clicked.connect(CriarModelo)
 primeira_tela.lineEdit_2.setEchoMode(QtWidgets.QLineEdit.Password)
 
 primeira_tela.show()
